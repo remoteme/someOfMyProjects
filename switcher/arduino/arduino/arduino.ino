81d5276a9ad6c284563a32d3dce0cdfa91212766
remoteme <maciej@mytimeplan.com>
2018-03-24 15:23:38 +0000

    update switcher

--- switcher/arduino/arduino/arduino.ino
+++ switcher/arduino/arduino/arduino.ino
@@ -3,6 +3,7 @@
 #define DEVICE_ID 202
 #define DEVICE_NAME "switcher"
 #define TOKEN "~1_VLTq=uGG@M1Hp"
+#define WEBPAGE_DEVICE_ID 1000
 
 #include <ArduinoHttpClient.h>
 #include <RemoteMe.h>
@@ -23,20 +24,14 @@
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 
-
-// Change these pin numbers to the pins connected to your encoder.
-//   Best Performance: both pins have interrupt capability
-//   Good Performance: only the first pin has interrupt capability
-//   Low Performance:  neither pin has interrupt capability
-Encoder knobLeft(D5, D6);
-
+Encoder encoder(D5, D6);
 
 
 uint8_t LEDpin = D3;
 int analogPin = A0;
 
-long positionLeft = -999;
-long positionRight = -999;
+
+
 
 
 RBD::Button button(D7);
@@ -49,7 +44,7 @@ Servo servo;
 
 void setup() {
 	Serial.begin(9600);
-	
+
 	WiFiMulti.addAP(WIFI_NAME, WIFI_PASSWORD);
 	while (WiFiMulti.run() != WL_CONNECTED) {
 		delay(100);
@@ -64,66 +59,107 @@ void setup() {
 	remoteMe.sendRegisterDeviceMessage(DEVICE_NAME);
 
 
-	servo.attach(2); //D4
+	servo.attach(15); //D8
 
-	servo.write(0);
+	servo.write(20);
 
 	//----- display
 	Wire.begin();
 	lcd.begin(16, 2); // Inicjalizacja LCD 2x16
 	lcd.setCursor(0, 0);
-	lcd.print("maciek");
+	lcd.print("RemoteMe.org");
+	lcd.setCursor(0, 1);
+	lcd.print(":)   :)   :)");
 }
 
 
 void onUserSyncMessage(uint16_t senderDeviceId, uint16_t dataSize, uint8_t* data, uint16_t &returnDataSize, uint8_t *&returnData) {
 
+
 }
 
+//1 string to display
+//2 servo
+//3 diode
 void onUserMessage(uint16_t senderDeviceId, uint16_t dataSize, uint8_t *data) {
 
+	char   buffer[25];
+	sprintf(buffer, "on message  %d %d", data[0], data[1]);
+	Serial.println(buffer);
 
+	uint16_t pos = 0;
+	uint8_t type = RemoteMeMessagesUtils::getByte(data, pos);
+
+	if (type == 1) {
+		String line1 = RemoteMeMessagesUtils::getString(data, pos);
+		String line2 = RemoteMeMessagesUtils::getString(data, pos);
+		lcd.setCursor(0, 0);
+		lcd.print(line1);
+		lcd.setCursor(0, 1);
+		lcd.print(line2);
+	}
+	else if (type == 2) {
+		servo.write(RemoteMeMessagesUtils::getByte(data, pos));
+	}
+	else if (type == 3) {
+		analogWrite(LEDpin, ((int)4)*RemoteMeMessagesUtils::getByte(data, pos));
+	}
 }
+
+//1 - Potentiometer + value
+//2 - button click 
+//3 - encoder + value - signed int
+void sendChangeEvent(uint8_t type, uint8_t value) {
+	uint16_t pos = 0;
+	uint8_t *data = (uint8_t*)malloc(2);
+	RemoteMeMessagesUtils::putByte(data, pos, type);
+	RemoteMeMessagesUtils::putByte(data, pos, value);
+
+	char   buffer[25];
+	sprintf(buffer, "send message  %d %d", data[0], data[1]);
+	Serial.println(buffer);
+	remoteMe.sendUserMessage(WEBPAGE_DEVICE_ID, data, 2);
+}
+
+
+uint16_t lastVoltage = 0;
+int32_t position = -999;
 
 int i = 0;
 void loop() {
 	remoteMe.loop();
 
-	long newLeft, newRight;
-	newLeft = knobLeft.read();
-	if (newLeft != positionLeft) {
-		if (newLeft % 4 == 0) {
+	int32_t newPosition;
+	newPosition = encoder.read();
+	if (newPosition != position) {
+		if (newPosition % 4 == 0) {
 			Serial.print("Left = ");
-			Serial.print(newLeft / 4);
+			Serial.println(newPosition / 4);
 
-			Serial.println();
+			sendChangeEvent(3, newPosition / 4);
+			//encoder.write(0);
 		}
 
-		positionLeft = newLeft;
-		positionRight = newRight;
+		position = newPosition;
+
 	}
 
 
 	if (button.onPressed()) {
-		Serial.println("Button Pressed");
+		Serial.println("Button1 Pressed");
+		sendChangeEvent(2, 0);
 	}
 
-	i++;
-	analogWrite(LEDpin, i);
-	delay(4);
-	if (i > 1024) {
+	
+	if (i++ > 10000) {//when we read analog to often esp8266 is disconecting its known issue :( https://github.com/esp8266/Arduino/issues/1634 //its run the code every 200ms
+		int currentPotentiometer = analogRead(analogPin);
+		if (abs(currentPotentiometer - lastVoltage) > 20) {
+			Serial.println(currentPotentiometer);
+			lastVoltage = currentPotentiometer;
+			sendChangeEvent(1, currentPotentiometer / 4);
+		}
 		i = 0;
-		Serial.println("reset");
+		//Serial.println(millis());
 	}
-
-	if (i < 512) {
-		servo.write(10);
-	}
-	else {
-		servo.write(200);
-	}
-
-	if (i % 256 == 0) {
-		Serial.println(analogRead(analogPin));
-	}
+	
 }
